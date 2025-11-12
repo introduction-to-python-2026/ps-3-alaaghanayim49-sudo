ea118c1f013462bca725e2aeae35b6c94566b423
alaaghanayim49-sudo <alaaghanayim49@gmail.com>
2025-11-12 13:52:56 +0200

    Update edge_case_exercise.py

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -6,9 +6,10 @@ def move(my_list, direction):
             my_list[index_of_one] = 0
             my_list[index_of_one + 1] = 1
 
+   
     elif direction == 'left':
         if index_of_one > 0:
             my_list[index_of_one] = 0
             my_list[index_of_one - 1] = 1
 
-    return my_list
+    return my_list
